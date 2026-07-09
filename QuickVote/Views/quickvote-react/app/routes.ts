3379384f6d2c8b5f29733090576750d8cc761bea
Jacuos <jacek.kozieja@gmail.com>
2026-07-09 15:05:58 +0200

    separate route for poll vote/results view

--- QuickVote/Views/quickvote-react/app/routes.ts
+++ QuickVote/Views/quickvote-react/app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  route,
+  index,
+  layout,
+  prefix,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("vote","routes/vote.tsx")
+] satisfies RouteConfig;
